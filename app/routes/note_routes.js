a7ee9534dd1a7a2156dceaee6bd407b6babbc17c
endorphin82 <endorphin82@gmail.com>
2018-06-18 04:39:13 +0300

    add server.js post /notes

---
+++ app/routes/note_routes.js
@@ -0,0 +1,8 @@
+module.exports = function(app, db) {
+    app.post('/notes', (req, res) => {
+        // Здесь будем создавать заметку.
+        console.log(req.body)
+
+        res.send('Hello')
+    });
+};
