4280ddac3007dda0f43ebc621656ccda3322db4c
endorphin82 <endorphin82@gmail.com>
2018-06-19 11:39:21 +0300

    add local mdb and all methods

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : 'mongodb://admin:123456@ds149763.mlab.com:49763/mordor_db'
+    // url : 'mongodb://admin:123456@ds149763.mlab.com:49763/mordor_db'
+    url : 'mongodb://@127.0.0.1:27017/notes_db'
 };
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
 const port = 8000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
     require('./app/routes')(app, database);
